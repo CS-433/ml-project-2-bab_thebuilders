d07ef55781e6631f8a192f93f3cd19205171a1cc
bastlan3 <bastien.lelan@yahoo.fr>
2021-12-23 10:56:32 +0100

    Update data.py

--- source/data.py
+++ source/data.py
@@ -243,7 +243,110 @@ def reduce_multiply(x,y, number) :
 
     return [np.array(image), np.array(mask)]
     
-    
+def blur(img,fact):
+    img3=ndimage.gaussian_filter(img, sigma=fact)
+    return img3
+
+def switcharoo(img):
+    img1=img.copy()
+    img1[:,:,0]=img[:,:,2]
+    img1[:,:,1]=img[:,:,0]
+    img1[:,:,2]=img[:,:,1]
+    img2=img.copy()
+    img2[:,:,0]=img[:,:,1]
+    img2[:,:,1]=img[:,:,2]
+    img2[:,:,2]=img[:,:,0]
+    return img1,img2
+
+def saturation(img,factor):
+    img4=Image.fromarray(img)
+    converter = ImageEnhance.Color(img4)
+    img4 = converter.enhance(factor)
+    img4=np.array(img4)
+    return img4
+
+def lightness(img, factor):
+    img2=Image.fromarray(img)
+    enhancer=ImageEnhance.Brightness(img2)
+    img_light = enhancer.enhance(factor)
+    img2=np.array(img_light)
+    return img2
+
+def sharpness(img, factor):
+    img2=Image.fromarray(img)
+    enhancer = ImageEnhance.Sharpness(img2)
+    img2=enhancer.enhance(factor)
+    img2=np.array(img2)
+    return img2
+
+def rotation_zoom(img,angle):
+    taille=len(img[:,0,0])
+    zoom=round((taille*(math.sin(math.radians(angle))+math.cos(math.radians(angle))))/taille,3)+0.001
+    img1=ndimage.zoom(img,(zoom,zoom,1))
+    img1=ndimage.rotate(img1, angle,reshape=False)
+    centre=(len(img1[:,0,0])/2)
+    gauche=int(centre-taille/2)
+    droite=int(centre+taille/2)
+    img1=img1[gauche:droite,gauche:droite,:]
+    return img1
+
+def shear_vertical(img1,factor):
+    H, W = img1[:,:,0].shape
+    zoom=round(1/(1-factor),3)
+    M2 = np.float32([[1, 0, 0], [factor, 1, 0]])
+    M2[0,2] = -M2[0,1] * W/2
+    M2[1,2] = -M2[1,0] * H/2
+    aff2 = cv2.warpAffine(img1, M2, (W, H))
+    gauche=int(H/2-H/2*(1-factor))
+    droite=int(H/2+H*(1-factor)/2)
+    aff2=aff2[gauche:droite,:,:]
+    aff2=ndimage.zoom(aff2,(zoom,1,1))
+    return aff2
+
+def shear_horizontal(img1,factor):
+    H, W = img1[:,:,0].shape
+    zoom=round(1/(1-factor),3)
+    M2 = np.float32([[1, factor, 0], [0, 1, 0]])
+    M2[0,2] = -M2[0,1] * W/2
+    M2[1,2] = -M2[1,0] * H/2
+    aff2 = cv2.warpAffine(img1, M2, (W, H))
+    gauche=int(H/2-H/2*(1-factor))
+    droite=int(H/2+H*(1-factor)/2)
+    aff2=aff2[:,gauche:droite,:]
+    aff2=ndimage.zoom(aff2,(1,zoom,1))
+    return aff2
+
+def boing(img,fact,Vertical):
+    vert=Vertical
+    class WaveDeformer:
+        def transform(self, x, y):
+            if vert:
+                x = x + fact*math.sin(x/40)
+            else:
+                y = y + fact*math.sin(x/40)
+            return x, y
+        def transform_rectangle(self, x0, y0, x1, y1):
+            return (*self.transform(x0, y0),
+                    *self.transform(x0, y1),
+                    *self.transform(x1, y1),
+                    *self.transform(x1, y0),
+                    )
+        def getmesh(self, img):
+            self.w, self.h = img.size
+            gridspace = 20
+            target_grid = []
+            for x in range(0, self.w, gridspace):
+                for y in range(0, self.h, gridspace):
+                    target_grid.append((x, y, x + gridspace, y + gridspace))
+
+            source_grid = [self.transform_rectangle(*rect) for rect in target_grid]
+
+            return [t for t in zip(target_grid, source_grid)]
+    img4=Image.fromarray(img)
+    result_image = ImageOps.deform(img4, WaveDeformer())
+    img2=np.array(result_image)
+    return img2
+ 
 def augment_data(images, masks, save_path, augment=True):
     """ Performing data augmentation. 
     Input : 
@@ -342,17 +445,80 @@ def augment_data(images, masks, save_path, augment=True):
             ## translate images
             x23 = np.roll(x, (-50,-50), axis=(0,1))
             y23 = np.roll(y, (-50,50), axis=(0,1))
+            
+            ## blur with gaussian filter
+            x24 = blur(x,2)
+            x25 = blur(x,4)
+            y24 = y
+            y25 = y
 
+            ## higher and lower saturation
+            x26=saturation(x,2)
+            y26=y
+            x27=saturation(x,0.5)
+            y27=y
+
+            ##higher and lower brightness
+            x28=lightness(x,2.5)
+            y28=y
+            x29=lightness(x,0.5)
+            y29=y
+
+            ## switch the RGB columns
+            x30,x31= switcharoo(x)
+            y30=y
+            y31=y
+
+            ## change the image sharpness
+            x32=sharpness(x,5)
+            y32=y
+            x33=sharpness(x,-4)
+            y33=y
+
+            ##vertical shear its possible to add .8 but the image is very deformed
+            x33=shear_vertical(x,0.2)
+            x34=boing(x,10, True)
+            x35=boing(x,30,True)
+
+            y33=shear_vertical(y,0.2)
+            y34=boing(x,10, True)
+            y35=boing(x,30,True)
+
+             ##horizontal shearits possible to add .8 but the image is very deformed
+            x37=shear_horizontal(x,0.2)
+            x38=boing(x,10, False)
+            x36=boing(x,30,False)
+
+            y37=shear_horizontal(y,0.2)
+            y38=boing(x,10, False)
+            y36=boing(x,30,False)
+
+            ##rotation zoom many rotation angle possible between 0 and 90
+            x41=rotation_zoom(x,20)
+            y41=rotation_zoom(y,20)
+
+            x42=rotation_zoom(x,40)
+            y42=rotation_zoom(y,40)
+
+            x39=rotation_zoom(x,60)
+            y39=rotation_zoom(y,60)
+
+            x40=rotation_zoom(x,80)
+            y40=rotation_zoom(y,80)
+
+            #the augmentations fro x24 to x42 did not bring significant improvement and were therefore not used for this model
             # build the images and masks arrays
             images = [
                 x, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10,
                 x11, x12, x13, x14, x15, x16, x17, x18, x19, x20,
-                x21,x22,x23
+                x21,x22,x23 #,x24,x25,x26,x27,x28,x29,x30,x31,x32,x33,
+                #x34,x35,x36,x37,x38,x39,x40,x41,x42
             ]
             masks  = [
                 y, y1, y2, y3, y4, y5, y6, y7, y8, y9, y10,
                 y11, y12, y13, y14, y15, y16, y17, y18, y19, y20,
-                y21,y22,y23
+                y21,y22,y23#,y24,y25,y26,y27,y28,y29,y30,y31,y32,y33,
+                #y34,y35,y36,y37,y38,y39,y40,y41,y42
             ]
 
         else:
