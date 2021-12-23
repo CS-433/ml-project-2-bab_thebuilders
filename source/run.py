a85e9203da15e1e1f08580989d3f39596aacf71b
bastlan3 <bastien.lelan@yahoo.fr>
2021-12-23 10:45:47 +0100

    Create run.py

---
+++ source/run.py
@@ -0,0 +1,27 @@
+from google.colab import drive
+drive.mount('/content/drive', force_remount=True)
+%cd "/content/drive/MyDrive/ML/"
+
+import data as data
+from glob import glob
+import model as Model
+import train as Train
+import trainTuning as tT
+import predict as Predict
+import pathlib
+import mask_to_submission as m2s
+import pandas as pd
+import numpy as np
+import predict as Predict
+import cv2
+
+
+    #data augmentation
+#data.augment_split()
+   
+    #model training
+#Train.main_train()
+    
+Predict.main_predict() 
+
+m2s.main_mask_to_submission()
